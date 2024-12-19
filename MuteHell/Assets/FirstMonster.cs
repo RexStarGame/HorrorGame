771eafdcd249bd331054b98de073a9eea822dc35
alvinulv <alvinulvb0159@gmail.com>
2024-12-19 09:12:49 +0100

    made the monster capable of targeting the player, and updated notes

--- MuteHell/Assets/FirstMonster.cs
+++ MuteHell/Assets/FirstMonster.cs
@@ -1,39 +1,40 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class FirstMonster : MonoBehaviour
 {
+    //default state for the monster is prowling
     public Transform[] waypoints;
-    //for now this is an int. 0 means the monster has no target, 1 means the player is the target
-    public int target;
+    public string target;
     NavMeshAgent monsterAI;
     [SerializeField] int currentIndex;
     // Start is called before the first frame update
     void Start()
     {
         monsterAI = GetComponent<NavMeshAgent>();
-        if (target == 0)
+        if (target == null)
             monsterAI.SetDestination(waypoints[currentIndex].position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, waypoints[currentIndex].position) < 0.3f && target == 0)
+        if (target == null && Vector3.Distance(transform.position, waypoints[currentIndex].position) < 0.3f)
         {
             Debug.Log("waypoint " + currentIndex + " er nået");
             currentIndex++;
             if (currentIndex == waypoints.Length)
-            {
                 currentIndex = 0;
-            }
             monsterAI.SetDestination(waypoints[currentIndex].position);
         }
         GameObject player = GameObject.FindWithTag("Player");
-        if (target == 1)
+        if (Vector3.Distance(transform.position, player.transform.position) < 5)
+            target = "Player";
+        if (target == "Player")
         {
             monsterAI.SetDestination(player.transform.position);
         }
